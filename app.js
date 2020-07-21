bf3dda8fc5d3f7112f0835fb8d25501d8864479d
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-20 22:42:34 -0700

    bus mall is hard

--- app.js
+++ app.js
@@ -9,30 +9,36 @@ const selectButton = document.getElementById('select');
 
 
 
-const secondPoke = pokeDisplay[1];
-const thirdPoke = pokeDisplay[2];
 
-// initialize state
-//let pokemonEncountered = 0;
-//let timesCaptured = 0;
-//let pokemonCaptured = [];
-//let notCaptured = [];
-// set event listeners to update state and DOM
-selectButton.addEventListener('click', () => {
+
+ //initialize state
+let pokemonEncountered = 0;
+let timesCaptured = 0;
+let pokemonCaptured = [];
+let notCaptured = [];
+ //set event listeners to update state and DOM
+function setPage() {
     
-    const randoPick = getRandomPokemon(pokeArray);
+    let randoPick = getRandomPokemon(pokeArray);
+    let randoPick2 = getRandomPokemon(pokeArray);
+    let randoPick3 = getRandomPokemon(pokeArray);
+    
+
+    while (randoPick.id === randoPick2.id || randoPick.id === randoPick3.id || randoPick2.id === randoPick3.id) {
+        randoPick2 = getRandomPokemon(pokeArray);
+        randoPick3 = getRandomPokemon(pokeArray);
+    }
+    
     const firstPoke = pokeDisplay[0];
     const input1 = firstPoke.children[0];
     const img1 = firstPoke.children[1];
     img1.src = randoPick.url_image;
 
-    const randoPick2 = getRandomPokemon(pokeArray);
     const secondPoke = pokeDisplay[1];
     const input2 = secondPoke.children[0];
     const img2 = secondPoke.children[1];
     img2.src = randoPick2.url_image;
 
-    const randoPick3 = getRandomPokemon(pokeArray);
     const thirdPoke = pokeDisplay[2];
     const input3 = thirdPoke.children[0];
     const img3 = thirdPoke.children[1];
@@ -40,4 +46,11 @@ selectButton.addEventListener('click', () => {
 
 
 
-});
+}
+
+//pokeRadio.addEventListener('checked', () => {}
+
+
+
+
+setPage();
