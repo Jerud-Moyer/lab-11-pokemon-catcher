4c3e4d4b0d8352434e516bd15b83a987e2b86845
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-20 19:05:15 -0700

    pokemon rendering!

--- app.js
+++ app.js
@@ -19,11 +19,25 @@ const thirdPoke = pokeDisplay[2];
 //let notCaptured = [];
 // set event listeners to update state and DOM
 selectButton.addEventListener('click', () => {
+    
     const randoPick = getRandomPokemon(pokeArray);
     const firstPoke = pokeDisplay[0];
-    const input1 = firstPoke.children[1];
-    const img1 = firstPoke.children[2];
-    console.log(randoPick);
-    //input1.textcontent = 
+    const input1 = firstPoke.children[0];
+    const img1 = firstPoke.children[1];
     img1.src = randoPick.url_image;
+
+    const randoPick2 = getRandomPokemon(pokeArray);
+    const secondPoke = pokeDisplay[1];
+    const input2 = secondPoke.children[0];
+    const img2 = secondPoke.children[1];
+    img2.src = randoPick2.url_image;
+
+    const randoPick3 = getRandomPokemon(pokeArray);
+    const thirdPoke = pokeDisplay[2];
+    const input3 = thirdPoke.children[0];
+    const img3 = thirdPoke.children[1];
+    img3.src = randoPick3.url_image;
+
+
+
 });
