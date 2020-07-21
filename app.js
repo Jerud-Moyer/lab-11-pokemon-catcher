9a074830b249944fa37903bcdf06885348c8cfd9
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-20 23:44:22 -0700

    ughh

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const pokeDisplay = document.querySelectorAll('label');
 const pokeRadio = document.querySelectorAll('input');
 const selectButton = document.getElementById('select');
 
+const firstPoke = pokeDisplay[0];
+const input1 = firstPoke.children[0];
+const secondPoke = pokeDisplay[1];
+const input2 = secondPoke.children[0];
+const thirdPoke = pokeDisplay[2];
+const input3 = thirdPoke.children[0];
+
 
 
 
@@ -29,28 +36,31 @@ function setPage() {
         randoPick3 = getRandomPokemon(pokeArray);
     }
     
-    const firstPoke = pokeDisplay[0];
-    const input1 = firstPoke.children[0];
+    //const firstPoke = pokeDisplay[0];
     const img1 = firstPoke.children[1];
     img1.src = randoPick.url_image;
+    firstPoke.children[2].textContent = randoPick.pokemon;
 
-    const secondPoke = pokeDisplay[1];
-    const input2 = secondPoke.children[0];
     const img2 = secondPoke.children[1];
     img2.src = randoPick2.url_image;
+    secondPoke.children[2].textContent = randoPick2.pokemon;
 
-    const thirdPoke = pokeDisplay[2];
-    const input3 = thirdPoke.children[0];
     const img3 = thirdPoke.children[1];
     img3.src = randoPick3.url_image;
+    thirdPoke.children[2].textContent = randoPick3.pokemon;
+
+    
 
 
 
 }
 
-//pokeRadio.addEventListener('checked', () => {}
-
-
-
-
 setPage();
+
+pokeRadio.addEventListener('click', () => {
+    
+    if (input1 === 'checked') {
+        firstPoke.push(pokemonCaptured);
+
+    console.log(pokemonCaptured);}
+});
